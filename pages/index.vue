1a47fc1e0d76a2761899d164c6b6859b02d277ce
rodentskie <rodentskie@gmail.com>
2021-05-19 06:10:07 +0800

    test cicd

--- pages/index.vue
+++ pages/index.vue
@@ -17,7 +17,7 @@
                 icon="plus"
                 small
                 class="icon"
-              />Add Employee
+              />Add Employee Test
             </b-button>
           </b-col>
 
